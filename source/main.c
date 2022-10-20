64a68cfa9fd402e0158b37dffad45015a9745cce
dezem <dezem@users.noreply.github.com>
2022-10-20 22:36:35 +0200

    add sleep...

--- source/main.c
+++ source/main.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <switch.h>
+#include "unistd.h"
 
 int main(int argc, char **argv)
 {
     // Console init
     spsmInitialize();
+    //sleep
+    usleep(3000000);
     // AMS safe shutdown
     spsmShutdown(false);
-    spsmExit();
-    return 0;
 }
